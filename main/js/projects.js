0b5226456c445684ab824b1116a01cf318beb57c
Alex <szelestey.alexander@gmail.com>
2020-09-11 12:22:33 -0400

    project page

---
+++ main/js/projects.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+var btn = document.getElementById("viewBtn");
+var modal = document.getElementById("hmsModal");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+span.onclick = function () {
+    modal.style.display = "none";
+}
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
